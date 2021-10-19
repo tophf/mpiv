tophf/mpiv

commit 482e499a031835dc1e47352c9f8b06652e8e4cd8

fix and simplify csp sniffer

script.user.js
@@ -717,17 +717,11 @@ const CspSniffer = {
 
   // will be null when done
   init() {
-    this.initPending = this.initPending || new Promise(resolve => {
-      GM.xmlHttpRequest({
-        url: location.href,
-        method: 'HEAD',
-        onload: response => {
-          this.csp = this._parse(response);
-          this.init = this.initPending = null;
-          resolve();
-        },
+    this.initPending = this.initPending ||
+      fetch(location, {method: 'HEAD'}).catch(() => false).then(r => {
+        this.csp = r && this._parse(r.headers.get('content-security-policy'));
+        this.init = this.initPending = null;
       });
-    });
   },
 
   async check(url) {
@@ -742,8 +736,7 @@ const CspSniffer = {
     return [mode || ai.xhr, isVideo];
   },
 
-  _parse({responseHeaders}) {
-    const csp = responseHeaders.match(/(?:^|[\r\n])\s*Content-Security-Policy:([^\r\n]*)/i);
+  _parse(csp) {
     if (!csp) return;
     const src = {};
     const rx = /[\s;](default|img|media)-src ([^;]+)/g;

commit 9fab8a247da2b7bebeb2f80e920fb56cf0689b37

1.2.11 actually parse CSP, regressed in 482e499a

script.user.js
@@ -22,7 +22,7 @@
 // @grant       GM.setValue
 // @grant       GM.xmlHttpRequest
 //
-// @version     1.2.10
+// @version     1.2.11
 // @author      tophf
 //
 // @original-version 2017.9.29
@@ -740,7 +740,7 @@ const CspSniffer = {
     if (!csp) return;
     const src = {};
     const rx = /[\s;](default|img|media)-src ([^;]+)/g;
-    for (let m; (m = rx.exec(csp[1]));)
+    for (let m; (m = rx.exec(csp));)
       src[m[1]] = m[2].trim().split(/\s+/);
     if (!src.img) src.img = src.default || [];
     if (!src.media) src.media = src.default || [];