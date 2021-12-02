cc9e90c866e38abd144823f3bc67108292efa99c
tophf <tophf@gmx.com>
2021-12-02 13:22:28 +0300

    comment

--- types/types.d.ts
+++ types/types.d.ts
@@ -70,7 +70,7 @@ declare namespace mpiv {
      - a function that returns a string or an array of such strings
     */
     s?: StringOrArrayOfStrings | SubstitutionFunction
-    // whether the resultant URL should be processed again so another rule would match it
+    // do GMxhr without setting/getting cookies
     anonymous?: boolean
     // whether the resultant URL should be processed again so another rule would match it
     follow?: boolean | BooleanFunction
