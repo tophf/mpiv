9c3b948119077163442eb08c97834328fa6432ba
tophf <tophf@gmx.com>
2022-06-15 18:55:18 +0300

    use jsdoc comments

--- types/types.d.ts
+++ types/types.d.ts
@@ -45,27 +45,27 @@ declare namespace mpiv {
   };
 
   type HostRule = {
-    // URL match using plain text and AdBlock-compatible special symbols ||, |, ^
+    /** URL match using plain text and AdBlock-compatible special symbols ||, |, ^ */
     u?: StringOrArrayOfStrings
-    /*
+    /**
      URL match using RegExp
      - when "html" is true, the node's HTML is matched instead of URL
      - when "u" is present, the "r" is checked only if "u" matched
     */
     r?: RegExp
-    // caption extractor: CSS selector or a function
+    /** caption extractor: CSS selector or a function */
     c?: string | CaptionFunction
-    // CSS selector for the hovered element
+    /** CSS selector for the hovered element */
     e?: string | string[] | Object<string,string>
-    // gallery
+    /** gallery */
     g?: GalleryLoader | GalleryFunction
-    /*
+    /**
      remote element extractor: CSS selector
      - applied to the DOM of the document downloaded from URL (note, "s" rule can change the URL)
      - can be an array of selectors which will be checked in the specified order until matched (unlike CSS selector 'foo, bar' here you control the priority)
     */
     q?: StringOrArrayOfStrings | QuerySelectorFunction
-    /*
+    /**
      URL substitution
      - a string used in RegExp#replace like 'http://foo/bar$1/$2'
      - an array of such strings, used in order until one succeeds
@@ -73,23 +73,23 @@ declare namespace mpiv {
      - a function that returns a string or an array of such strings
     */
     s?: StringOrArrayOfStrings | SubstitutionFunction
-    // do GMxhr without setting/getting cookies
+    /** do GMxhr without setting/getting cookies */
     anonymous?: boolean
-    // whether the resultant URL should be processed again so another rule would match it
+    /** whether the resultant URL should be processed again so another rule would match it */
     follow?: boolean | BooleanFunction
-    // POST method should be used to make HTTP request
+    /** POST method should be used to make HTTP request */
     post?: boolean | BooleanFunction
-    // CSS selector for an element used to calculate the bounds of the hoverable area
+    /** CSS selector for an element used to calculate the bounds of the hoverable area */
     rect?: string
-    // CSS to be added to the page
+    /** CSS to be added to the page */
     css?: string
-    // match in outerHTML string
+    /** match in outerHTML string */
     html?: boolean
-    // shows the popup only when user activates it explicitly e.g. via a key
+    /** shows the popup only when user activates it explicitly e.g. via a key */
     manual?: boolean
-    // Firefox-only fix needed with some obstinate hostings when opening image in a tab via "T" key
+    /** Firefox-only fix needed with some obstinate hostings when opening image in a tab via "T" key */
     tabfix?: boolean
-    // spoof anti-hotlinking protection
+    /** spoof anti-hotlinking protection */
     xhr?: boolean
   }
 
@@ -109,31 +109,31 @@ declare namespace mpiv {
   type PopupInfo = {
     bar: Element
     blobUrl: string
-    // buffering bar shown
+    /** buffering bar shown */
     bufBar: boolean
-    // buffering start time, ms
+    /** buffering start time, ms */
     bufStart: number
     caption: string
-    // video controls are shown
+    /** video controls are shown */
     controlled: boolean
-    // clientX of the last mouse event
+    /** clientX of the last mouse event */
     cx: number
-    // clientY of the last mouse event
+    /** clientY of the last mouse event */
     cy: number
     extras: {
-      // padding + border for left and right edges
+      /** padding + border for left and right edges */
       inw: number,
-      // padding + border for top and bottom edges
+      /** padding + border for top and bottom edges */
       inh: number,
-      // margin + outline for left and right edges
+      /** margin + outline for left and right edges */
       outw: number,
-      // margin + outline for top and bottom edges
+      /** margin + outline for top and bottom edges */
       outh: number,
-      // padding + margin + border + outlines for left and right edges
+      /** padding + margin + border + outlines for left and right edges */
       w: number,
-      // padding + margin + border + outlines for top and bottom edges
+      /** padding + margin + border + outlines for top and bottom edges */
       h: number,
-      // outline offset + thickness
+      /** outline offset + thickness */
       o: number,
     }
     flipX: boolean
@@ -144,27 +144,27 @@ declare namespace mpiv {
     gNum: number
     imageUrl: StringOrArrayOfStrings
     large: boolean
-    // naturalHeight
+    /** naturalHeight */
     nheight: number
-    // naturalWidth
+    /** naturalWidth */
     nwidth: number
     popup: HTMLImageElement | HTMLVideoElement
-    // true when 'load' event fired on the element
+    /** true when 'load' event fired on the element */
     popupLoaded: boolean
-    // time, ms
+    /** time, ms */
     preloadStart: number
-    // used by gallery to preload the next image
+    /** used by gallery to preload the next image */
     preloadUrl: string
     rect: DOMRect
-    // is mouse still over PopupInfo.rect
+    /** is mouse still over PopupInfo.rect */
     rectHovered: boolean
     req: { abort: VoidFunction }
     rotate: number
     scale: number
     scales: number[]
-    // scale fit-to-window factor
+    /** scale fit-to-window factor */
     scaleFit: number
-    // scale factor to use when zoom is enabled
+    /** scale factor to use when zoom is enabled */
     scaleZoom: number
     shiftKeyTime: number
     timer: number
@@ -183,22 +183,21 @@ declare namespace mpiv {
   }
 
   type GalleryItemsArray = GalleryItem[] & {
-    // e.g. let g=[GalleryItem, GalleryItem]; g.title='foo'
+    /** e.g. let g=[GalleryItem, GalleryItem]; g.title='foo' */
     title?: string
-    //
     index?: IndexFunction | string | number
   }
 
   type GalleryLoader = {
-    // CSS selector: entry (required to extract captions)
+    /** CSS selector: entry (required to extract captions) */
     entry?: string
-    // CSS selector: image (relative to the entry element if "entry" is specified)
+    /** CSS selector: image (relative to the entry element if "entry" is specified) */
     image?: string
-    // CSS selector: caption (relative to the entry element)
+    /** CSS selector: caption (relative to the entry element) */
     caption?: StringOrArrayOfStrings
-    // CSS selector: title of the gallery
+    /** CSS selector: title of the gallery */
     title?: string
-    // function to transform URLs/captions/title
+    /** function to transform URLs/captions/title */
     fix?: { (s: string, isURL: boolean): string }
   }
 
@@ -255,10 +254,14 @@ declare namespace mpiv {
 
   enum StartEnum {
     auto,
-    context, // historical name for Context menu + Ctrl
-    contextMK, // context menu invoked via Mouse or Keyboard
-    contextM, // context menu invoked via Mouse
-    contextK, // context menu invoked via Keyboard
+    /** historical name for Context menu + Ctrl */
+    context,
+    /** context menu invoked via Mouse or Keyboard */
+    contextMK
+    /** context menu invoked via Mouse */,
+    contextM,
+    /** context menu invoked via Keyboard */
+    contextK,
     ctrl,
   }
   type Start = keyof typeof StartEnum
